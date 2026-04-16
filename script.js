8651549cd1f8549d28a544a8a6fd9dc3f1304e3a
rayanebenoudia <rayane.benoudia@laplateforme.io>
2026-04-16 10:01:18 +0200

    je push

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
+
 const accueil1 = document.getElementById('link-home')
 const contact1 = document.getElementById('link-contact')
 
 const pages ={ accueil: "<h1>Bienvenue sur l'Accueil</h1><p>C'est la page principale.</p>",
-                contact: "<h1>Contactez-nous</h1><p>Voici un formulaire imaginaire...</p>"
+    contact: "<h1>Contactez-nous</h1><p>Voici un formulaire imaginaire...</p>"
 }
 const rootdiv = document.getElementById("root")
+
+
 
 function changerpage(nomDeLaPage) {
     rootdiv.innerHTML=pages[nomDeLaPage]
     
 }
 contact1.addEventListener('click',function(e){
+    console.log("L'URL actuelle est :", window.location.pathname)
+    history.pushState('null','','contact')
+    console.log(history)
     e.preventDefault()
     changerpage('contact')
+    console.log(history.state)
 })
 accueil1.addEventListener('click',function(e){
-e.preventDefault()
+    console.log("L'URL actuelle est :", window.location.pathname)
+    
+e.preventDefault() 
+history.pushState('null','','home')
 changerpage('accueil')
 })
+console.log("L'URL actuelle est :", window.location.pathname)
+
